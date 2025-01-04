f234d11500b55ef39b04bd551e2ae880de6d68c9
MisbahAnjumShaikh <misbahanjumshaikh05pk@gmail.com>
2025-01-04 11:55:01 +0500

    sixth-sec cards UI

--- src/components/SixthWebAndNative.jsx
+++ src/components/SixthWebAndNative.jsx
@@ -18,7 +18,7 @@ const SixthWebAndNative = () => {
         <div className="d-flex gap-2 cards">
         <SixthCards sixthCardsHeading={"Stay true to the web"} className="webCard" sixthCardsPara={"People expect web app pages to load fast. On the server, React lets you start streaming HTML while you’re still fetching data, progressively filling in the remaining content before any JavaScript code loads. On the client, React can use standard web APIs to keep your UI responsive even in the middle of rendering."}/>
 
-        <SixthCards sixthCardsHeading={"Go truly native"} sixthCardsPara={"People expect native apps to look and feel like their platform." <a>React Native</a>  "and" Expo "let you build apps in React for Android, iOS, and more. They look and feel native because their UIs are truly native. It’s not a web view—your React components render real Android and iOS views provided by the platform."}/>
+        {/*<SixthCards sixthCardsHeading={"Go truly native"} sixthCardsPara={"People expect native apps to look and feel like their platform." <a>React Native</a>  "and" Expo "let you build apps in React for Android, iOS, and more. They look and feel native because their UIs are truly native. It’s not a web view—your React components render real Android and iOS views provided by the platform."}/>*/}
         </div>
       </section>
     </Fragment>
